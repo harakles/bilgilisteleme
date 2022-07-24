cf7856a9fd6af42f8e032ec1595f7b7cd7714136
harakles <mustafablkfb2004@gmail.com>
2022-07-24 16:08:58 +0300

    sss
    sss

--- BilgiListeleme/BilgiListeleme/Controllers/GuvenlikController.cs
+++ BilgiListeleme/BilgiListeleme/Controllers/GuvenlikController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using BilgiListeleme.Models;
 
 namespace BilgiListeleme.Controllers
@@ -19,8 +20,9 @@ namespace BilgiListeleme.Controllers
         public ActionResult GirisYap(TBLAdmin t)
         {
             var bilgiler = db.TBLAdmin.FirstOrDefault(x => x.AdminUserName == t.AdminUserName && x.AdminPw == t.AdminPw);
-            if(bilgiler != null)
+            if(bilgiler !=null)
             {
+                FormsAuthentication.SetAuthCookie(bilgiler.AdminUserName, false);
                 return RedirectToAction("Index","Home");
             }
             else
