14e8335823cd6b98b93152b8fb3473777cb63871
harakles <mustafablkfb2004@gmail.com>
2022-07-24 06:24:38 +0300

    asd
    asd

---
+++ BilgiListeleme/BilgiListeleme/Controllers/GuvenlikController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BilgiListeleme.Models;
+
+namespace BilgiListeleme.Controllers
+{
+    public class GuvenlikController : Controller
+    {
+        DBBilgiListelemeEntities1 db = new DBBilgiListelemeEntities1();
+        // GET: Guvenlik
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLAdmin t)
+        {
+            var bilgiler = db.TBLAdmin.FirstOrDefault(x => x.AdminUserName == t.AdminUserName && x.AdminPw == t.AdminPw);
+            if(bilgiler != null)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
--- BilgiListeleme/BilgiListeleme/Controllers/HomeController.cs
+++ BilgiListeleme/BilgiListeleme/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace BilgiListeleme.Controllers
     public class HomeController : Controller
     {
         DBBilgiListelemeEntities1 db = new DBBilgiListelemeEntities1();
+        [Authorize]
         public ActionResult Index(string p, int sayfa = 1)
         {
 
@@ -26,17 +27,13 @@ namespace BilgiListeleme.Controllers
             //var datas = db.TBLCustomerVdsList.ToList().ToPagedList(sayfa, 8);
             //return View(datas);
         }
-        public ActionResult RemoteDesk(string p , int sayfa = 1)
+        public ActionResult RemoteDesk(int Id)
         {
+
+            var yks = db.TBLCustomerVdsList.Find(Id);
             
-            var Datalar = from k in db.TBLCustomerVdsList select k;
-            if (!string.IsNullOrEmpty(p))
-            {
-
-
-                Datalar = Datalar.Where(m => m.Sirketİsmi.Contains(p));
-            }
-            return View(Datalar.ToList().ToPagedList(sayfa, 1));
+            return View(yks);
         }
+        
     }
 }
