478ca45aafe2f8f196894719e4d24cba1521081a
harakles <55509530+harakles@users.noreply.github.com>
2022-07-27 14:48:52 +0300

    2022.02.27
    14.48

---
+++ BilgiListeleme2/BilgiListeleme2/Controllers/GuvenlikController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using BilgiListeleme2.Models;
+
+namespace BilgiListeleme.Controllers
+{
+    public class GuvenlikController : Controller
+    {
+        DBBilgiListelemeEntities db = new DBBilgiListelemeEntities();
+        // GET: Guvenlik
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLAdmin t)
+        {
+            var bilgiler = db.TBLAdmin.FirstOrDefault(x => x.AdminUserName == t.AdminUserName && x.AdminPw == t.AdminPw);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.AdminUserName, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BilgiListeleme2/BilgiListeleme2/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using System.Web.Security;
+using PagedList.Mvc;
+using BilgiListeleme2.Models;
+
+namespace BilgiListeleme.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBBilgiListelemeEntities db = new DBBilgiListelemeEntities();
+        [Authorize]
+        public ActionResult Index(string p, int sayfa = 1)
+        {
+
+            var Datalar = from k in db.TBLCustomerVdsList select k;
+            if (!string.IsNullOrEmpty(p))
+            {
+
+
+                Datalar = Datalar.Where(m => m.Sirketİsmi.Contains(p));
+            }
+            return View(Datalar.ToList().ToPagedList(sayfa, 6));
+            //var datas = db.TBLCustomerVdsList.ToList().ToPagedList(sayfa, 8);
+            //return View(datas);
+        }
+        public ActionResult RemoteDesk(int Id)
+        {
+
+            var yks = db.TBLCustomerVdsList.Find(Id);
+
+            return View(yks);
+        }
+        [HttpGet]
+        public ActionResult VeriEkle()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult VeriEkle(TBLCustomerVdsList p)
+        {
+
+            db.TBLCustomerVdsList.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
+        }
+
+    }
+}
